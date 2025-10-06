38a0d920c5764625a9477f1a992a995652fc9346
Stefano Casasola <ste.casasola@gmail.com>
2025-10-06 11:11:52 +0200

    Adds more ES6 features

--- scripts/es6.js
+++ scripts/es6.js
@@ -89,3 +89,103 @@ person.bark2()
 // è l'intera finestra del browser (non possiede piena consapevolezza di sé).
 
 // le funzioni freccia hanno una scorciatoia per RITORNARE dei valori
+const stefify = function (str) {
+  return 'stefano' + str
+}
+
+// se la vostra funzione fa UNA SOLA COSA (ha una riga) e in questa riga ritorna un
+// valore, voi la potete abbreviare così:
+
+const stefifyArrow = (str) => 'stefano' + str
+// stefifyArrow fa LA STESSA COSA di stefify ma in una sola riga!
+// ricordate che la funzione deve fare una sola cosa (avere una sola riga)
+// e ritornare un valore: a quel punto potete abbreviarla in una funzione freccia
+// in cui omettete SIA la parola "return" SIA le graffe
+stefifyArrow('casasola') // ritorna 'stefanocasasola'
+
+// un'altra novità di ES6: lo SPREAD OPERATOR
+// lo spread operator è una tecnica per CLONARE efficacamente strutture dati
+// complesse come array e oggetti
+
+let a = 10
+let b = a // crea una variabile b con un valore SEPARATO dal valore di a
+
+a = 1
+// b è rimasto 10
+
+let objA = { counter: 10 }
+let objB = objA // PROBLEMA! objB NON È UNA DIVERSA ALLOCAZIONE DI MEMORIA
+objA.counter = 1
+// objB.counter è diventato 1 pure lui! il valore era lo stesso condiviso
+
+let arrA = ['cane', 'gatto']
+let arrB = arrA // non è una vera copia! il valore è comune
+arrA.pop()
+// anche in arrB non c'è più il gatto, la lunghezza di entrambi è diventata 1
+
+// per creare delle VERE copie di entità complesse (array e oggetti) potete
+// utilizzare lo SPREAD OPERATOR
+
+let objectA = { name: 'Stefano' }
+let objectB = { ...objectA } // questo è un NUOVO oggetto con dentro trasportate
+// una copia di TUTTE le proprietà di objectA
+objectB.name = 'Piero' // non avete cambiato la proprietà name di objectA
+
+// utilizziamo ora lo spread operator per creare un oggetto con tutte le proprietà
+// di objectA e objectB
+let objectC = { ...objectA, ...objectB } // { name: 'Stefano', name: 'Piero' }
+// però non possiamo in JS avere un oggetto in cui due proprietà si chiamano uguali!
+// risultato -> { name: 'Piero' }
+
+const objC = {
+  name: 'Stefano',
+  name: 'Piero',
+} // objC una volta creato ha UNA proprietà "name" con valore "Piero"
+
+let objectD = { ...objectA, objectB }
+// { name: 'Stefano', objectB: {name: 'Piero'} }
+// JSON.parse(JSON.stringify(ARRAYDIOGGETTI))
+
+const arrayA = ['cane', 'gatto']
+const arrayB = [...arrayA] // una vera e propria copia!
+arrayA.pop()
+// arrayB ha ANCORA LUNGHEZZA 2
+
+//
+// OBJECT DESTRUCTURING (destrutturazione di oggetti)
+const person1 = {
+  firstName: 'Al',
+  lastName: 'Bano',
+  age: 100,
+}
+
+// const firstName = person1.firstName
+// la potete fare anche così:
+
+const { firstName, lastName, age } = person1
+// ho creato una variabile "firstName" che ha valore person1.firstName
+// ho creato una variabile "lastName" che ha valore person1.lastName
+// ho creato una variabile "age" che ha valore person1.age
+
+// pura scorciatoia, se vi fa venire il mal di testa fate a meno! :)
+
+// TEMPLATE LITERALS (concatenazione easy di stringhe)
+// per parlarne introduciamo il TERZO DELIMITATORE DI STRINGHE in JS!
+
+const str = `
+ciao mi chiamo Stefano
+e insegno JS
+`
+// 1) con i backtick si possono fare stringhe su più righe
+// 2) con i backtick possiamo INTERPOLARE variabili dentro alle stringhe
+
+const myFirstName = 'Stefano'
+const myLastName = 'Morandi'
+const myAge = 60
+
+const result1 =
+  'Io mi chiamo ' + myFirstName + ' ' + myLastName + ' e ho ' + myAge + 'anni.'
+
+const result2 = `Io mi chiamo ${myFirstName} ${myLastName} e ho ${myAge} anni.`
+
+// result1 === result2
